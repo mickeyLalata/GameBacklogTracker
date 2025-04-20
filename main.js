7fa7987763bd4b82eb9196fbe4d13e7223ccdbcd
mickeyLalata <mckyl1108@gmail.com>
2025-04-21 02:29:56 +0800

    added touchmove function and also forgot to include in pass commit in css added mobile view interface

---
+++ main.js
@@ -0,0 +1,46 @@
+const contentList = document.getElementById("container");
+const gameList = document.getElementById("list");
+const activeList = document.querySelectorAll(".list-view");
+const currentlyPlaying = document.getElementById("playing");
+const activeCurrent = document.querySelectorAll(".play-view");
+const donePlaying = document.getElementById("done");
+const activeDone = document.querySelectorAll(".done-view");
+const contList = document.getElementById("game-list");
+const contCurrentPlaying = document.getElementById("current-playing");
+const contDonePlaying = document.getElementById("done-playing");
+const rstBtn = document.getElementById("reset");
+
+const activeScroll = (scroll) => {
+scroll.forEach(touch => {
+    touch.addEventListener("touchmove", () => {
+        scroll.forEach(el => el.classList.remove("active"));
+        touch.classList.add("active");
+    });
+    touch.addEventListener("touchend", () => {
+        touch.classList.remove("active");
+      });
+    
+    touch.addEventListener("touchcancel", () => {
+        touch.classList.remove("active");
+      });
+});
+};
+
+activeScroll(activeList);
+activeScroll(activeCurrent);
+activeScroll(activeDone);
+
+gameList.addEventListener("click", () => {
+    contList.style.display = "block";
+    contentList.style.display = "none";
+    rstBtn.addEventListener("click", () => {
+        contList.style.display = "none";
+        contentList.style.display = "flex";
+    });
+});
+currentlyPlaying.addEventListener("click", () => {
+    alert("Div Clicked!!!");
+});
+donePlaying.addEventListener("click", () => {
+    alert("Div Clicked!!!");
+});
